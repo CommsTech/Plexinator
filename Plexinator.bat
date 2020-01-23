416ce3e8086eb8ec719b3a81c55ba4e3ae997709
CommsTech <skaterass87@gmail.com>
2020-01-23 12:36:13 -0700

    Update Plexinator.bat
    Added option to for targeted use

--- Plexinator.bat
+++ Plexinator.bat
@@ -48,27 +48,72 @@ IF "%LIBARYCHECK%"=="" SET LIBARYCHECK=E:\Plexinator\LibaryCheck.ps1
 
 CD /D %WORK_DIR%
 
+:Menu
+echo
+echo 1. By the numbers
+echo 2. Automagic
+set /p MENUSELECT=Type Your Choice then press ENTER :
+IF %MENUSELECT%==1 goto Submenu
+IF %MENUSELECT%==2 goto Automagic
+
+:Submenu
+echo
+echo 1. Converter
+echo 2. Optimizer
+echo 3. Filer
+echo 4. Tester
+echo 5. Automagic
+
+set /p SUBMENUSELECT=Type Your Choice then press ENTER :
+IF %SUBMENUSELECT%==1 goto Converter
+IF %SUBMENUSELECT%==2 goto Optimizer
+IF %SUBMENUSELECT%==3 goto Filer
+IF %SUBMENUSELECT%==4 goto Tester
+IF %SUBMENUSELECT%==5 goto Automagic
+
+
 :Converter
 CLS
 Title Plexinator - Handbreak Conversion (Step 2)
 echo Lets start with file conversions
 FOR /F "tokens=*" %%G IN ('DIR /B /S *.ts -or *.avi -or *.mov -or *.m4v -or *.flv -or *.MPV -or *.MPEG -or *.WMV') DO "%HANDBRAKE_CLI%" -i "%%G" -o "%OUTPUT_DIR%\%%~nG.mp4" --preset="Fast 1080p30" --optimize
+goto submenu
 
 :Optimizer
 CLS
 Title Plexinator - FFMPEG optimizer (Step 3)
 echo time to optimize exhisting videos
 ECHO N | FOR /F "tokens=*" %%G IN ('DIR /B /S *.mp4') DO "%FFMPG%" -i "%%G" -movflags faststart -acodec copy -vcodec copy "%OUTPUT_DIR%\%%~nG.mp4" -threads 0
+goto submenu
 
 :Filer
 CLS
 Title Plexinator - FileBot Orginiation (Step 4)
 echo lets put those files where they belong
 FOR /F "tokens=*" %%G IN ('DIR /B /S *.mp4') DO "%FILEBOT%" -rename "%%G" -script fn:amc --output "%OUTPUT_DIR%" --action move --conflict skip -non-strict --log-file amc.log --def unsorted=n music=y artwork=n clean=y movieFormat="%OUTPUT_DIR%\Movies\{n} ({y})\{n} ({y})" seriesFormat="%OUTPUT_DIR%\TV Shows\{n} - {episode.special ? 'S00E'+special.pad(2) : s00e00} - {t.replaceAll(/[`´‘’ʻ]/, /'/).replaceAll(/[!?.]+$/).replacePart(', Part $1')}{'.'+lang}" "ut_label=%L" "ut_state=%S" "ut_title=%N" "ut_kind=%K" "ut_file=%F" "ut_dir=%D"
+goto submenu
 
-:tester
+:Tester
 CLS
 Title Plexinator - Handbreak Tester (Step 5)
 echo Time to list the files with possible playback issues
 Powershell.exe -File "%LIBARARYCHECK%"
+goto submenu
+
+:Automagic
+echo Automagiclly starting
+Title Plexinator - Handbreak Conversion (Step 2)
+echo Lets start with file conversions
+FOR /F "tokens=*" %%G IN ('DIR /B /S *.ts -or *.avi -or *.mov -or *.m4v -or *.flv -or *.MPV -or *.MPEG -or *.WMV') DO "%HANDBRAKE_CLI%" -i "%%G" -o "%OUTPUT_DIR%\%%~nG.mp4" --preset="Fast 1080p30" --optimize
+Title Plexinator - FFMPEG optimizer (Step 3)
+echo time to optimize exhisting videos
+ECHO N | FOR /F "tokens=*" %%G IN ('DIR /B /S *.mp4') DO "%FFMPG%" -i "%%G" -movflags faststart -acodec copy -vcodec copy "%OUTPUT_DIR%\%%~nG.mp4" -threads 0
+Title Plexinator - FileBot Orginiation (Step 4)
+echo lets put those files where they belong
+FOR /F "tokens=*" %%G IN ('DIR /B /S *.mp4') DO "%FILEBOT%" -rename "%%G" -script fn:amc --output "%OUTPUT_DIR%" --action move --conflict skip -non-strict --log-file amc.log --def unsorted=n music=y artwork=n clean=y movieFormat="%OUTPUT_DIR%\Movies\{n} ({y})\{n} ({y})" seriesFormat="%OUTPUT_DIR%\TV Shows\{n} - {episode.special ? 'S00E'+special.pad(2) : s00e00} - {t.replaceAll(/[`´‘’ʻ]/, /'/).replaceAll(/[!?.]+$/).replacePart(', Part $1')}{'.'+lang}" "ut_label=%L" "ut_state=%S" "ut_title=%N" "ut_kind=%K" "ut_file=%F" "ut_dir=%D"
+Title Plexinator - Handbreak Tester (Step 5)
+echo Time to list the files with possible playback issues
+Powershell.exe -File "%LIBARARYCHECK%"
+goto Menu
+
 pause
