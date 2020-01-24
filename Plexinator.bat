fc0975230a63342988219efec57709c26db97472
CommsTech <skaterass87@gmail.com>
2020-01-23 19:37:25 -0700

    House Cleaning
    Removed excess files and reduced load times

--- Plexinator.bat
+++ Plexinator.bat
@@ -32,10 +32,39 @@ REM ----------------------------------------------------------------------------
 CLS
 Title Plexinator - Setup (Step 1)
 REM set program locations
+:Variables
+REM ------------------------------------------------------------------------------------------------------------------------
+REM Gather information and set Variables
+REM ------------------------------------------------------------------------------------------------------------------------
+
+CLS
+Title Plexinator - Setup (Step 1)
+REM set program locations
+Echo Do You Have All the Prereqs? i.e. Handbreakcli,ffmpeg and filebot?
+Echo 1. Yes
+Echo 2. No
+SET /P PREREQ=
+IF %PREREQ%==1 Goto START 
+IF %PREREQ%==2 Powershell.exe -executionpolicy remotesigned -File PreReq_Downloader.ps1
+
+:START
+SET /P OUTPUT_DIR=Enter your Video file output directory here:
+IF "%OUTPUT_DIR%"=="" SET OUTPUT_DIR=D:\Downloads\Completed
+SET /P HANDBRAKE_CLI=Enter the full handbreakcli.exe location ex: X:\mine\handbreakcli.exe here:
+IF "%HANDBRAKE_CLI%"=="" SET HANDBRAKE_CLI=E:\Plexinator\PREREQS\HandBrakeCLI.exe
+SET /P FFMPG=Enter the full ffmpeg.exe location ex: X:\mine\ffmpeg.exe here:
+IF "%FFMPG%"=="" SET FFMPG=E:\Plexinator\PREREQS\ffmpeg.exe
+SET /P FILEBOT=Enter the full filebot.exe location ex: X:\mine\filebot.exe here:
+IF "%FILEBOT%"=="" SET FILEBOT=C:\Users\terra\AppData\Local\Microsoft\WindowsApps\PointPlanck.FileBot_49ex9gnthnt12\filebot
+SET /P LIBARYCHECK=Enter the full Libarycheck.ps1 location ex:X:\mine\LibaryCheck.ps1 here:
+IF "%LIBARYCHECK%"=="" SET LIBARYCHECK=E:\Plexinator\PREREQS\LibaryCheck.ps1
+SET /a THREADA=%NUMBER_OF_PROCESSORS% / 2 + (%NUMBER_OF_PROCESSORS%/2/2)
+IF /I "%THREADA%" LEQ "1" SET /a THREADA=0
+SET /P THREADS=Enter the number of threads you want to use ( We recommend %THREADA% )
+IF "%THREADS%"=="" SET THREADS=%THREADA%
 echo What is the directory of your video files
 SET /P WORK_DIR=Enter your directory here:
 IF "%WORK_DIR%"=="" SET WORK_DIR=C:\
-for /f "tokens=1 delims=" %%i in (Settings.conf) do echo %%i
 
 CD /D %WORK_DIR%
 
